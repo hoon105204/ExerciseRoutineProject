2395c2187306bea85f0f5ad6c0807e3795e311c7
hoon105204 <hoon105204@gmail.com>
2022-06-20 18:08:52 +0900

    update

---
+++ ExerciseRoutine/src/test/java/com/excercise/routine/ExerciseRoutineApplicationTests.java
@@ -0,0 +1,13 @@
+package com.excercise.routine;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExerciseRoutineApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
