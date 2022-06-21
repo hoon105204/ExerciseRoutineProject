0fdb9837f7507236aa3d93ddbe36df02e6d49c9a
YangSeongHui <shxoxo0731@gmail.com>
2022-06-22 01:06:55 +0900

    exboard con, mapper, views

--- ExerciseRoutine/src/main/java/com/excercise/routine/exboard/mapper/ExboardMapper.java
+++ ExerciseRoutine/src/main/java/com/excercise/routine/exboard/mapper/ExboardMapper.java
@@ -13,20 +13,19 @@ import com.excercise.routine.exboard.dto.ExboardDto;
 @Mapper
 public interface ExboardMapper {
    
-   @Select(" SELECT BNO, USERNAME, TITLE, CONTENT, BDATE FROM EXBOARD ORDER BY BNO DESC ")
-   public List<ExboardDto> selectList();
-   
-   @Select(" SELECT BNO, USERNAME, TITLE, CONTENT, BDATE FROM EXBOARD WHERE BNO = #{bno} ")
-   public ExboardDto selectOne(int bno);
-   
-   @Insert(" INSERT INTO EXBOARD VALUES(NULL, #{username}, #{title}, #{content}, NOW()) ")
-   public int insert(ExboardDto dto);
-   
-   @Update(" UPDATE EXBOARD SET TITLE = #{title}, CONTENT = #{content} WHERE BNO = #{bno} ")
-   public int update(ExboardDto dto);
-   
-   @Delete(" DELETE FROM EXBOARD WHERE BNO = #{bno} ")
-   public int delete(int bno);
+	@Select(" SELECT BNO, USERNAME, TITLE, CONTENT, BDATE FROM EXBOARD ORDER BY BNO DESC ")
+	public List<ExboardDto> selectList();
 
-   
+	@Select(" SELECT BNO, USERNAME, TITLE, CONTENT, BDATE FROM EXBOARD WHERE BNO = #{bno} ")
+	public ExboardDto selectOne(int bno);
+
+	@Insert(" INSERT INTO EXBOARD VALUES(NULL, #{username}, #{title}, #{content}, NOW()) ")
+	public int insert(ExboardDto dto);
+
+	@Update(" UPDATE EXBOARD SET TITLE = #{title}, CONTENT = #{content} WHERE BNO = #{bno} ")
+	public int update(ExboardDto dto);
+
+	@Delete(" DELETE FROM EXBOARD WHERE BNO = #{bno} ")
+	public int delete(int bno);
+
 }
