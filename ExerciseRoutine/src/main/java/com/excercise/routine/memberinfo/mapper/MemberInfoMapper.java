b6bfdcee5197752e0208efcdc0d715a6bf37752d
malut98 <aokimki1@naver.com>
2022-06-21 09:44:27 +0900

    mapper2

--- ExerciseRoutine/src/main/java/com/excercise/routine/memberinfo/mapper/MemberInfoMapper.java
+++ ExerciseRoutine/src/main/java/com/excercise/routine/memberinfo/mapper/MemberInfoMapper.java
@@ -1,5 +1,26 @@
 package com.excercise.routine.memberinfo.mapper;
 
-public class MemberInfoMapper {
+import java.util.List;
 
+import org.apache.ibatis.annotations.Insert;
+import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Select;
+import org.apache.ibatis.annotations.Update;
+
+import com.excercise.routine.memberinfo.dto.MemberInfoDto;
+
+@Mapper
+public interface MemberInfoMapper {
+	
+	@Select(" SELECT * FROM MEMBERINFO ORDER BY USERID DESC ")
+	List<MemberInfoDto> selectList();
+	
+	@Select(" SELECT * FROM MEMBERINFO WHERE USERID=? ")
+	public MemberInfoDto selectOne(String USERID);
+	
+	@Insert(" INSERT INTO MEMBERINFO VALUES(?, ?, ?, ?, ?, ?, ?) ")
+	public int insert(MemberInfoDto dto);
+	
+	@Update(" UPDATE MEMBERINFO SET HEIGHT=? WHERE USERID=? ")
+	public int update(MemberInfoDto dto);
 }
