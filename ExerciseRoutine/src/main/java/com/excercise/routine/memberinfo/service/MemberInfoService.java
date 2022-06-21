8c5813d84e20bd6e73daa1c796bb5c0c6393309f
malut98 <aokimki1@naver.com>
2022-06-21 09:37:00 +0900

    service

--- ExerciseRoutine/src/main/java/com/excercise/routine/memberinfo/service/MemberInfoService.java
+++ ExerciseRoutine/src/main/java/com/excercise/routine/memberinfo/service/MemberInfoService.java
@@ -1,5 +1,13 @@
 package com.excercise.routine.memberinfo.service;
 
+import java.util.List;
+
+import com.excercise.routine.memberinfo.dto.MemberInfoDto;
+
 public interface MemberInfoService {
+	public List<MemberInfoDto> selectList();
+	public MemberInfoDto selectOne(String USERID);
+	public int insert(MemberInfoDto dto);
+	public int update(MemberInfoDto dto);
 	
 }
