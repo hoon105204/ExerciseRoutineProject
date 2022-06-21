f39b0f5cab4e69f75b0ffd783135717872b48c31
malut98 <aokimki1@naver.com>
2022-06-21 10:25:19 +0900

    sd

--- ExerciseRoutine/src/main/java/com/excercise/routine/ExerciseRoutineApplication.java
+++ ExerciseRoutine/src/main/java/com/excercise/routine/ExerciseRoutineApplication.java
@@ -2,6 +2,7 @@ package com.excercise.routine;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @SpringBootApplication
 public class ExerciseRoutineApplication {
@@ -9,5 +10,8 @@ public class ExerciseRoutineApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(ExerciseRoutineApplication.class, args);
 	}
-
+	@GetMapping("/")
+	public String root() {
+		return "index";
+	}
 }
